d47dc6d3611831519ccf80fb365362aaa73739fa
Justin Goro <justingoro@protonmail.com>
2020-09-11 21:27:27 +0200

    values fixed for rounding

--- etherscanscrape.js
+++ etherscanscrape.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 
-const holdingsCSV = fs.readFileSync('./holders as of 14:38 GMT+2.csv', 'utf8')
+const holdingsCSV = fs.readFileSync('./holders as of 11 September 20:21 (GMT+2).csv', 'utf8')
 
 let addresses1 = []
 let values1 = []
@@ -12,9 +12,10 @@ let values3 = []
 
 const lines = holdingsCSV.split('\n')
 let third = Math.floor(lines.length / 3)
+console.log('third: ' + third)
 let number = 0
 let i = 0;
-for (; i < 35; i++) {
+for (; i < third; i++) {
     let line = lines[i]
 
     let lineArray = line.split(',')
@@ -24,10 +25,10 @@ for (; i < 35; i++) {
 
     const smallerValue = lineArray[4].substring(0, lineArray[4].length - 10)
     number += parseInt(smallerValue)
-    values1.push(smallerValue)
+    values1.push(parseInt(smallerValue))
 
 }
-for (; i < 70; i++) {
+for (; i < third * 2; i++) {
     let line = lines[i]
     if (!line)
         continue;
@@ -37,7 +38,7 @@ for (; i < 70; i++) {
     addresses2.push(lineArray[0])
     const smallerValue = lineArray[4].substring(0, lineArray[4].length - 10)
     number += parseInt(smallerValue)
-    values2.push(smallerValue)
+    values2.push(parseInt(smallerValue))
 }
 for (; i < lines.length - 1; i++) {
     let line = lines[i]
@@ -49,13 +50,32 @@ for (; i < lines.length - 1; i++) {
         continue
     addresses3.push(lineArray[0])
     let smallerValue = lineArray[4].substring(0, lineArray[4].length - 10)
-    if (i === lines.length - 2)
-        smallerValue -= 1
+
     number += parseInt(smallerValue)
-    values3.push(smallerValue)
+    values3.push(parseInt(smallerValue))
 }
 console.log(number)
+//]                49813305801518
+let difference = 50000000000000 - number -1
+console.log(difference)
+const additionDivisor = values1.length + values3.length + values2.length
+let add = difference / additionDivisor
+let trueNumber = 0
 
+for (let j = 0; j < values1.length; j++) {
+    values1[j] += add
+    trueNumber +=  values1[j]
+}
+for (let j = 0; j < values2.length; j++) {
+    values2[j] += add
+    trueNumber +=  values2[j]
+}
+for (let j = 0; j < values3.length; j++) {
+    values3[j] += add
+    trueNumber +=  values3[j]
+}
+
+console.log(trueNumber)
 const outputLines = ['Holder addresses:', JSON.stringify(addresses1), JSON.stringify(addresses2), JSON.stringify(addresses3), 'Values (wei)', JSON.stringify(values1), JSON.stringify(values2), JSON.stringify(values3)]
 const outputText = outputLines.join('\n')
 fs.writeFileSync('./temp/fields.txt', outputText)
